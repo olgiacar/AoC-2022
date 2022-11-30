1ec047b4fe4ad807fbfb88e2a0fc0fb22afddc97
Carlos Olgiati <olgiacar@students.zhaw.ch>
2022-11-30 14:31:09 +0100

    Copied setup from last year Updated Kotlin and Gradle versions

---
+++ src/7/Solution.kt
@@ -0,0 +1,24 @@
+package `7`
+
+import SolutionInterface
+
+class Solution : SolutionInterface(testSolutionOne = 7, testSolutionTwo = 5) {
+
+    override fun exerciseOne(input: List<String>): Int {
+        return input.map { it.toInt() }
+            .zipWithNext { a, b -> if (b > a) 1 else 0 }
+            .sum()
+    }
+
+    override fun exerciseTwo(input: List<String>): Int {
+        return input.asSequence()
+            .map { it.toInt() }
+            .windowed(3)
+            .map { it.sum() }
+            .zipWithNext { a, b -> if (b > a) 1 else 0 }
+            .sum()
+    }
+
+}
+
+private fun main() = Solution().run()
---
+++ src/SolutionInterface.kt
@@ -0,0 +1,43 @@
+import java.io.File
+
+abstract class SolutionInterface(
+    private val testSolutionOne: Int,
+    private val testSolutionTwo: Int
+) {
+    private val inputPath = "${javaClass.packageName}/.input"
+    private val testPath = "${javaClass.packageName}/.test"
+
+    abstract fun exerciseOne(input: List<String>): Int
+    abstract fun exerciseTwo(input: List<String>): Int
+
+    fun run() {
+        checkTestInput()
+        runSolution()
+    }
+
+    private fun runSolution() {
+        val lines = readLines(inputPath)
+        val solutionOne = exerciseOne(lines)
+        val solutionTwo = exerciseTwo(lines)
+        printSolutions(solutionOne, solutionTwo)
+    }
+
+    private fun checkTestInput() {
+        val lines = readLines(testPath)
+        val one = exerciseOne(lines)
+        val two = exerciseTwo(lines)
+        println("Expected: $testSolutionOne, got: $one")
+        check(one == testSolutionOne)
+        println("Expected: $testSolutionTwo, got: $two")
+        check(two == testSolutionTwo)
+    }
+
+    private fun readLines(fileName: String): List<String> {
+        return File("src", fileName).readLines()
+    }
+
+    private fun printSolutions(one: Int, two: Int) {
+        println("Exercise one: $one")
+        println("Exercise two: $two")
+    }
+}
