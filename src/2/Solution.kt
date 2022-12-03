06e7072007654fea9e5a8a0abdc1a90fa7d26311
Carlos Olgiati <olgiacar@students.zhaw.ch>
2022-12-03 06:35:44 +0100

    Added solution three

--- src/2/Solution.kt
+++ src/2/Solution.kt
@@ -4,61 +4,54 @@ import SolutionInterface
 
 class Solution : SolutionInterface(testSolutionOne = 15, testSolutionTwo = 12) {
 
-    override fun exerciseOne(input: List<String>): Int {
-        return input.map { it.split(" ") }
-            .map { Pair(it.first(), it.last()) }
-            .sumOf { getPointsOne(it) }
-    }
+    override fun exerciseOne(input: List<String>): Int = input.map { it.split(" ") }
+        .map { Pair(it.first(), it.last()) }
+        .sumOf { getPointsOne(it) }
 
-    override fun exerciseTwo(input: List<String>): Int {
-        return input.map { it.split(" ") }
-            .map { Pair(it.first(), it.last()) }
-            .sumOf { getPointsTwo(it) }
-    }
+    override fun exerciseTwo(input: List<String>): Int = input.map { it.split(" ") }
+        .map { Pair(it.first(), it.last()) }
+        .sumOf { getPointsTwo(it) }
 
-    private fun getPointsTwo(moves: Pair<String, String>): Int {
-        return when (moves.second) {
-            "X" -> 0 + when (moves.first) {
-                "A" -> 3
-                "B" -> 1
-                else -> 2
 
-            }
+    private fun getPointsTwo(moves: Pair<String, String>): Int = when (moves.second) {
+        "X" -> 0 + when (moves.first) {
+            "A" -> 3
+            "B" -> 1
+            else -> 2
 
-            "Y" -> 3 + when (moves.first) {
-                "A" -> 1
-                "B" -> 2
-                else -> 3
-            }
+        }
 
-            else -> 6 + when (moves.first) {
-                "A" -> 2
-                "B" -> 3
-                else -> 1
-            }
+        "Y" -> 3 + when (moves.first) {
+            "A" -> 1
+            "B" -> 2
+            else -> 3
+        }
+
+        else -> 6 + when (moves.first) {
+            "A" -> 2
+            "B" -> 3
+            else -> 1
         }
     }
 
 
-    private fun getPointsOne(moves: Pair<String, String>): Int {
-        return when (moves.second) {
-            "X" -> 1 + when (moves.first) {
-                "A" -> 3
-                "B" -> 0
-                else -> 6
-            }
+    private fun getPointsOne(moves: Pair<String, String>): Int = when (moves.second) {
+        "X" -> 1 + when (moves.first) {
+            "A" -> 3
+            "B" -> 0
+            else -> 6
+        }
 
-            "Y" -> 2 + when (moves.first) {
-                "A" -> 6
-                "B" -> 3
-                else -> 0
-            }
+        "Y" -> 2 + when (moves.first) {
+            "A" -> 6
+            "B" -> 3
+            else -> 0
+        }
 
-            else -> 3 + when (moves.first) {
-                "A" -> 0
-                "B" -> 6
-                else -> 3
-            }
+        else -> 3 + when (moves.first) {
+            "A" -> 0
+            "B" -> 6
+            else -> 3
         }
     }
 }
--- src/3/Solution.kt
+++ src/3/Solution.kt
@@ -2,23 +2,44 @@ package `3`
 
 import SolutionInterface
 
-class Solution : SolutionInterface(testSolutionOne = 7, testSolutionTwo = 5) {
-
+class Solution : SolutionInterface(testSolutionOne = 157, testSolutionTwo = 70) {
     override fun exerciseOne(input: List<String>): Int {
-        return input.map { it.toInt() }
-            .zipWithNext { a, b -> if (b > a) 1 else 0 }
-            .sum()
+        return input.map { it.take(it.length / 2) to it.drop(it.length / 2) }
+            .map { getCommon(it) }
+            .sumOf { getPriority(it) }
     }
 
     override fun exerciseTwo(input: List<String>): Int {
-        return input.asSequence()
-            .map { it.toInt() }
-            .windowed(3)
-            .map { it.sum() }
-            .zipWithNext { a, b -> if (b > a) 1 else 0 }
-            .sum()
+        return input.chunked(3).map { Triple(it.first(), it[1], it.last()) }
+            .map { getCommon(it) }
+            .sumOf { getPriority(it) }
     }
 
+    private fun getCommon(rucksack: Pair<String, String>): Char {
+        val x = rucksack.first.toSet()
+        val y = rucksack.second.toSet()
+
+        val common = x.intersect(y)
+        return common.first();
+    }
+
+    private fun getCommon(rucksack: Triple<String, String, String>): Char {
+        val x = rucksack.first.toSet()
+        val y = rucksack.second.toSet()
+        val z = rucksack.third.toSet()
+
+        val common = x.intersect(y).intersect(z)
+        return common.first()
+    }
+
+    private fun getPriority(letter: Char): Int {
+        val offset = 'a'.code - 1
+        for (c in 'a'..'z') {
+            if (letter == c) return c.code - offset
+            if (letter == c.uppercaseChar()) return c.code - offset + 26
+        }
+        return 0
+    }
 }
 
 private fun main() = Solution().run()
