92184dc7fe405574ca3f82e823ea0eae42363ae2
Carlos Olgiati <olgiacar@students.zhaw.ch>
2022-12-08 07:18:57 +0100

    Added solution eight

--- src/7/Solution.kt
+++ src/7/Solution.kt
@@ -3,48 +3,40 @@ package `7`
 import SolutionInterface
 import isNumeric
 
-abstract class Dir(open val name: String) {
-    open val size get(): Int = 0
+class Directory(
+    private val name: String,
+    val children: MutableList<Directory> = mutableListOf(),
+    val parent: Directory? = null
+) {
+    val directorySize get(): Int = children.sumOf { it.fileSize } + fileSize
+    var fileSize = 0
 
-    open fun getFolders(): List<Folder> = listOf()
-}
+    fun getChild(n: String) = children.first { it.name == n }
 
-class Folder(
-    override val name: String,
-    val children: MutableList<Dir> = mutableListOf()
-    val parent: Folder? = null
-) : Dir(name) {
-    override val size get(): Int = children.sumOf { it.size }
-
-    fun getChild(n: String) = children.first { it.name == n } as Folder
-
-    override fun getFolders(): List<Folder> = mutableListOf(this).apply {
-        addAll(children.map { it.getFolders() }.flatten())
+    fun getDirectories(): List<Directory> = mutableListOf(this).apply {
+        addAll(children.map { it.getDirectories() }.flatten())
     }
 }
-
-class File(override val name: String, override val size: Int) : Dir(name)
-
 class Solution : SolutionInterface(testSolutionOne = 95437, testSolutionTwo = 24933642) {
 
-    override fun exerciseOne(input: List<String>) = buildFileSystem(input).getFolders()
-        .map { it.size }
+    override fun exerciseOne(input: List<String>) = buildFileSystem(input).getDirectories()
+        .map { it.directorySize }
         .filter { it <= 100_000 }
         .sum()
 
     override fun exerciseTwo(input: List<String>): Int {
         val root = buildFileSystem(input)
 
-        val needed = 30_000_000 - 70_000_000 + root.size
+        val needed = 30_000_000 - 70_000_000 + root.directorySize
 
-        return root.getFolders()
-            .map { it.size }
+        return root.getDirectories()
+            .map { it.directorySize }
             .filter { it >= needed }
             .min()
     }
 
-    private fun buildFileSystem(input: List<String>): Folder {
-        val root = Folder("/")
+    private fun buildFileSystem(input: List<String>): Directory {
+        val root = Directory("/")
         var current = root
         input.forEach {
             if (it.startsWith("$ cd")) current = navigate(current, it.substring(5))
@@ -53,7 +45,7 @@ class Solution : SolutionInterface(testSolutionOne = 95437, testSolutionTwo = 24
         return root
     }
 
-    private fun navigate(current: Folder, name: String): Folder {
+    private fun navigate(current: Directory, name: String): Directory {
         return when (name) {
             ".." -> current.parent!!
             "/" -> current
@@ -61,9 +53,9 @@ class Solution : SolutionInterface(testSolutionOne = 95437, testSolutionTwo = 24
         }
     }
 
-    private fun add(current: Folder, tokens: List<String>) {
-        if (tokens.first().isNumeric()) current.children.add(File(tokens.last(), tokens.first().toInt()))
-        else current.children.add(Folder(tokens.last(), parent = current))
+    private fun add(current: Directory, tokens: List<String>) {
+        if (tokens.first().isNumeric()) current.fileSize += tokens.first().toInt()
+        else current.children.add(Directory(tokens.last(), parent = current))
     }
 }
 
--- src/8/Solution.kt
+++ src/8/Solution.kt
@@ -1,23 +1,62 @@
 package `8`
 
 import SolutionInterface
+import kotlin.math.max
 
-class Solution : SolutionInterface(testSolutionOne = 7, testSolutionTwo = 5) {
+class Solution : SolutionInterface(testSolutionOne = 21, testSolutionTwo = 8) {
 
     override fun exerciseOne(input: List<String>): Int {
-        return input.map { it.toInt() }
-            .zipWithNext { a, b -> if (b > a) 1 else 0 }
-            .sum()
+        val forest = input.map { it.map { digit -> digit.digitToInt() } }
+        var visible = 0
+
+        for (i in 1..forest.size - 2) {
+            for (j in 1..forest.size - 2) {
+                if (isVisible(forest, i, j)) visible++
+            }
+        }
+
+        return forest.size * 4 - 4 + visible
     }
 
     override fun exerciseTwo(input: List<String>): Int {
-        return input.asSequence()
-            .map { it.toInt() }
-            .windowed(3)
-            .map { it.sum() }
-            .zipWithNext { a, b -> if (b > a) 1 else 0 }
-            .sum()
+        val forest = input.map { it.map { digit -> digit.digitToInt() } }
+        var max = 0
+
+        for (i in forest.indices) {
+            for (j in forest.indices) {
+                getScenicScore(forest, i, j).also { max = max(it, max) }
+            }
+        }
+
+        return max
     }
+
+    private fun isVisible(forest: List<List<Int>>, i: Int, j: Int): Boolean {
+        val value = forest[i][j]
+        if (forest[i].take(j).max() < value) return true
+        if (forest[i].takeLast(forest.size - j - 1).max() < value) return true
+        val column = getColumn(forest, j)
+        if (column.take(i).max() < value) return true
+        return column.takeLast(forest.size - i - 1).max() < value
+    }
+
+    private fun getScenicScore(forest: List<List<Int>>, i: Int, j: Int): Int {
+        val value = forest[i][j]
+        val left = getDistance(forest[i].take(j).reversed(), value)
+        val right = getDistance(forest[i].takeLast(forest.size - j - 1), value)
+        val column = getColumn(forest, j)
+        val top = getDistance(column.take(i).reversed(), value)
+        val bottom = getDistance(column.takeLast(forest.size - i - 1), value)
+        return left * right * bottom * top
+    }
+
+    private fun getDistance(trees: List<Int>, x: Int): Int {
+        val nonBlockingCount = trees.takeWhile { it < x }.size
+        if (nonBlockingCount == trees.size) return nonBlockingCount
+        return nonBlockingCount + 1
+    }
+
+    private fun getColumn(forest: List<List<Int>>, column: Int) = forest.map { it[column] }
 
 }
 
